8904c4be0c3933dc4a2d1a2f36d1a4c2af6790d6
chrislrjames <chrisjames20@gmail.com>
2021-03-10 11:53:30 -0500

    React Navbar Test Tested the React Navbar installation and integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import "./styles.css";
-import Navbar from "./navbar";
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
+
+import { Test } from "./test";
+
+import "../styles/styles.scss";
+import "../styles/app.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Navbar />
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home" style={{ fontSize: "30" }}>
+          <img
+            alt=""
+            src="/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          CJay Designs
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Test />
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Test() {
+  return (
+    <div className="test">
+      <h1>Test</h1>
+    </div>
+  );
+}
