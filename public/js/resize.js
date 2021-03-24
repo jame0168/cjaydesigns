e55eaf73bf31d492945c9e411a80f0b49250d6c0
chrislrjames <chrisjames20@gmail.com>
2021-03-24 11:25:01 -0400

    Scroll and Resize Animations Added in scroll.js and scroll.scss to handle scroll animations (not used now) and added in resize.js to handle adding a class to my screen div which we will use to handle animation and transition on the JavaScript side.

---
+++ public/js/resize.js
@@ -0,0 +1,19 @@
+const $ = window.$;
+
+function transitionSize() {
+  var windowTop = $(window).scrollTop() + 130;
+  var section = $(".section--one");
+  if (section.length) {
+    var top = section.offset().top;
+    if (windowTop > top) {
+      $(".screen").addClass("resize");
+    } else {
+      $(".screen").removeClass("resize");
+    }
+  }
+}
+
+$(function () {
+  $(window).scroll(transitionSize);
+  transitionSize();
+});
