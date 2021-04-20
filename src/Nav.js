a56426839a7510e966b137aec273353bbcc5201d
chrislrjames <chrisjames20@gmail.com>
2021-04-20 12:28:31 -0400

    Particle Image Refactoring Move Particle Image into its own file for better compartmentalization.

--- src/Nav.js
+++ src/Nav.js
@@ -1,32 +1,8 @@
 import React from "react";
 
-import ParticleImage, {
-  ParticleOptions,
-  Vector,
-  forces,
-  ParticleForce
-} from "react-particle-image";
+import { ParticleImg } from "./components/particleImage";
+
 import { Container, Nav, Navbar } from "react-bootstrap";
-
-const particleOptions: ParticleOptions = {
-  filter: ({ x, y, image }) => {
-    // Get pixel
-    const pixel = image.get(x, y);
-    // Make a particle for this pixel if blue > 50 (range 0-255)
-    return pixel.b > 50;
-  },
-  color: ({ x, y, image }) => "#333333",
-  radius: () => Math.random() * 1.5 + 0.5,
-  mass: () => 50,
-  friction: () => 0.15,
-  initialPosition: ({ canvasDimensions }) => {
-    return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
-  }
-};
-
-const motionForce = (x: number, y: number): ParticleForce => {
-  return forces.disturbance(x, y, 10);
-};
 
 export function NavBar() {
   return (
@@ -44,16 +20,7 @@ export function NavBar() {
               height="60"
               className="d-inline-block align-top"
             /> */}
-            <ParticleImage
-              src={"/img/cjaydesigns-grey-logo.png"}
-              scale={0.75}
-              entropy={20}
-              maxParticles={3000}
-              particleOptions={particleOptions}
-              mouseMoveForce={motionForce}
-              touchMoveForce={motionForce}
-              backgroundColor="transparent"
-            />
+            <ParticleImg />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse
