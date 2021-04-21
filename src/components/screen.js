7db1d836b3d51c605081dd9c859accda0bb239ed
chrislrjames <chrisjames20@gmail.com>
2021-04-20 23:42:04 -0400

    GSAP Animation Update Finished GSAP animation for Coding section. Finished desktop to mobile animation. Will need to work on making it responsive.

--- src/components/screen.js
+++ src/components/screen.js
@@ -6,54 +6,33 @@ import { Row, Col } from "react-bootstrap";
 
 export function Screen() {
   // const [screen, setScreen] = useState(".screen");
+  gsap.registerPlugin(ScrollTrigger);
+
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    gsap.from(".screen", {
-      scrollTrigger: {
-        trigger: ".screen",
-        start: "top 130px",
-        end: "+=1250",
-        toggleActions: "play none none reverse"
-      },
-      duration: 1,
-      boxShadow: "rgb(0 0 0/ 0%) 0px 0px 0px 0px"
-    });
-
-    gsap.to(".screen", {
-      scrollTrigger: {
-        trigger: ".screen",
-        start: "top -250px",
-        end: "+=1250",
-        scrub: true
-      },
-      duration: 3,
-      width: 280,
-      height: 475,
-      borderRadius: 30
-    });
-
-    gsap.to(".fauxHeader", {
-      scrollTrigger: {
-        trigger: ".screen",
-        start: "top 130px",
-        end: "+=1250",
-        toggleActions: "play none none reverse"
-      },
-      duration: 1,
-      marginTop: 0,
-      ease: "bounce.out"
-    });
-
     var animateIn = gsap.timeline({
       scrollTrigger: {
         trigger: ".screen",
         start: "top 130px",
         end: "+=1250",
         markers: true,
-        toggleActions: "play none none reverse"
+        toggleActions: "play none none none"
       }
     });
+
+    animateIn.from(".screen", {
+      duration: 1,
+      boxShadow: "rgb(0 0 0/ 0%) 0px 0px 0px 0px"
+    });
+
+    animateIn.to(
+      ".fauxHeader",
+      {
+        duration: 1,
+        marginTop: 0,
+        ease: "bounce.out"
+      },
+      "-=1"
+    );
 
     animateIn.fromTo(
       ".fauxImages",
@@ -63,8 +42,7 @@ export function Screen() {
       {
         scale: 1,
         duration: 1,
-        ease: "bounce.out",
-        delay: 1
+        ease: "bounce.out"
       }
     );
 
@@ -77,7 +55,8 @@ export function Screen() {
         width: "100%",
         duration: 1,
         ease: "bounce.out"
-      }
+      },
+      "-=1"
     );
 
     animateIn.fromTo(
@@ -86,9 +65,78 @@ export function Screen() {
         width: 0
       },
       {
-        width: 150,
+        width: 134,
         duration: 1,
         ease: "bounce.out"
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    var animateScrub = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".screen",
+        start: "top -250px",
+        end: "+=1250",
+        scrub: true
+      }
+    });
+
+    animateScrub.to(".screen", {
+      duration: 3,
+      width: 280,
+      height: 475,
+      borderRadius: 30
+    });
+
+    animateScrub.to(".fauxHeader", {
+      duration: 2,
+      height: "50%",
+      ease: "power2.inOut"
+    });
+
+    animateScrub.to(
+      ".fauxUser",
+      {
+        duration: 2,
+        x: -115,
+        y: 105,
+        scale: 4,
+        ease: "power2.inOut"
+      },
+      "-=1"
+    );
+
+    animateScrub.to(
+      ".leftAnimation",
+      {
+        duration: 2,
+        maxWidth: 0,
+        ease: "power2.inOut"
+      },
+      "-=2"
+    );
+
+    animateScrub.to(
+      ".rightAnimation",
+      {
+        duration: 2,
+        minWidth: "95%",
+        ease: "power2.inOut"
+      },
+      "-=1"
+    );
+
+    animateScrub.fromTo(
+      ".fauxIconContainer",
+      {
+        y: 60,
+        opacity: 0
+      },
+      {
+        y: -6,
+        opacity: 1,
+        ease: "power2.inOut"
       }
     );
   }, []);
@@ -105,17 +153,24 @@ export function Screen() {
         </div>
       </div>
       <Row>
-        <Col xs={12} md={5} className="pr-0 leftAnimation">
+        <Col xs={5} className="pr-0 leftAnimation">
           <div className="p-3">
             <div className="fauxImages"></div>
           </div>
         </Col>
-        <Col xs={12} md={7} className="pl-0 rightAnimation">
+        <Col xs={7} className="pl-0 rightAnimation">
           <div className="p-3">
             <div className="fauxText fauxTitle"></div>
             <div className="fauxText fauxContent"></div>
             <div className="fauxText fauxContent"></div>
             <div className="fauxText fauxContent"></div>
+            <div className="fauxText fauxContent"></div>
+            <div className="fauxIconContainer p-2 w-100 d-flex justify-content-between">
+              <div className="fauxIcons"></div>
+              <div className="fauxIcons"></div>
+              <div className="fauxIcons"></div>
+              <div className="fauxIcons"></div>
+            </div>
           </div>
         </Col>
       </Row>
